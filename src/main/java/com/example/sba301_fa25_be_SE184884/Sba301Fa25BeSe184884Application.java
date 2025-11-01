73ce40aa5f9f1c951b398ad52ecf29ac42c83dcb
phnanhky <kypase184884@fpt.edu.vn>
2025-11-01 07:47:26 +0700

    Initial commit

---
+++ src/main/java/com/example/sba301_fa25_be_SE184884/Sba301Fa25BeSe184884Application.java
@@ -0,0 +1,13 @@
+package com.example.sba301_fa25_be_SE184884;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sba301Fa25BeSe184884Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sba301Fa25BeSe184884Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sba301_fa25_be_SE184884/Sba301Fa25BeSe184884ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sba301_fa25_be_SE184884;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba301Fa25BeSe184884ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
